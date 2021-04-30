6f8fc65b78c390e7f87f31cbeeea5563659feccb
ABskr <bskr26@gmx.de>
2021-04-30 16:29:40 +0200

    populate removed

--- src/services/products/schema.js
+++ src/services/products/schema.js
@@ -10,7 +10,7 @@ const ProductSchema = new Schema(
       required: true,
       trim: true
     },
-    imageUrl : { type: String, required: true},
+    imageUrl : String,
     price : {type: Number, required: true},
     caterogy: String,
     reviews: [{
@@ -31,13 +31,13 @@ const ProductSchema = new Schema(
   })
 
 ProductSchema.static('findProductWithReviews', async function(productId){
-    const product = await this.findOne({ _id: productId}.populate('reviews'))
+    const product = await this.findOne({ _id: productId})
     return product
 })
 
 ProductSchema.static('findProductsWithReviews', async function(query){
   const total = await this.countDocuments(query.criteria)
-  const products = await this.find(query.criteria).skip(query.options.skip).limit(query.options.limit).sort(query.options.sort).populate('reviews')
+  const products = await this.find(query.criteria).skip(query.options.skip).limit(query.options.limit).sort(query.options.sort)
 
   return { total, products }
 })
