fcfceb5cf8da0321b022148d5695f94527445dbd
ABskr <bskr26@gmx.de>
2021-04-30 14:12:56 +0200

    another fix

--- src/services/reviews/schema.js
+++ src/services/reviews/schema.js
@@ -1,10 +1,11 @@
-import { Schema, model } from 'mongoose'
+import mongoose from 'mongoose'
 
-const ReviewSchema = new Schema(
+
+const ReviewSchema = new mongoose.Schema(
   {
     comment : {type: String, required: true, trim: true},
     rate :{ type: Number, min: 1, max: 5, required: true}
   }, { timestamps: true}
 )
 
-export default model('Review', ReviewSchema)
+export default mongoose.model('Review', ReviewSchema)
