2508838dd089c40e3c26cdf476c59166c4eb114e
ABskr <bskr26@gmx.de>
2021-04-30 13:18:58 +0200

    product's index updated

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import q2m from 'query-to-mongo'
 
-import ProductSchema from './schema.js'
+import ProductModel from './schema.js'
 const productsRouter = express.Router()
 
 
@@ -19,7 +19,7 @@ productsRouter.get('/', async (req, res, next) => {
 
 productsRouter.get('/:productId', async (req, res, next) => {
   try {
-    const product = await ProductSchema.findProductWithReviews(req.params.productId)
+    const product = await productsModel.findProductWithReviews(req.params.productId)
     if (!product) {
       const error = new Error()
       error.httpStatusCode = 404
@@ -34,22 +34,38 @@ productsRouter.get('/:productId', async (req, res, next) => {
 
 productsRouter.post('/', async (req, res, next) => {
   try {
-    
+    const newProduct = new ProductModel(req.body)
+    const {_id} = await newProduct.save()
+    res.status(201).send(`data is saved with id: ${_id}`)
   } catch (error) {
-
+    console.log(error)
+    next(error)
   }
 })
 
 productsRouter.put('/:productId', async (req, res, next) => {
   try {
-
+    const product = await productsModel.findByIdAndUpdate(req.params.productId, req.body, {runValidators: true, new: ture})
+    if (!product) {
+      const error = new Error(`UserId ${req.params.id} is not found!`)
+      error.httpStatusCode = 404
+      return next(error)
+    }
+    res.send(product)
   } catch (error) {
-
+    console.log(error)
+    next(error)
   }
 })
 
-productsRouter.delete('/', async (req, res, next) => {
+productsRouter.delete('/:productId', async (req, res, next) => {
   try {
+    const product = await ProductModel.findByIdAndDelete(req.params.productId)
+    if (!product) {
+      res.status(404).send(`User with id ${req.params.id} not found`)
+    } else {
+      res.send(`${req.params.id} is deleted!`)
+    }
 
   } catch (error) {
 
